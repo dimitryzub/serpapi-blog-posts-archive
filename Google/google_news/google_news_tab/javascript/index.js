090cad9b5a1237b58c54707fc1170ae04e9d87b7
Mikhail Zub <mikhail.a.zub@gmail.com>
2022-05-19 10:38:46 +0300

    Revert "finish writing"
    This reverts commit 9493532eb223b26cf2f0044d72391b3dedc4e4ec.

--- Google/google_news/google_news_tab/javascript/index.js
+++
@@ -1,6 +0,0 @@
-const withCheerio = require("./withCheerio");
-const withSerpApi = require("./withSerpApi");
-
-withCheerio.getNewsInfo().then(console.log);
-
-withSerpApi.getResults().then(console.log);
