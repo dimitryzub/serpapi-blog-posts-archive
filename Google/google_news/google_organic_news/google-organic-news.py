077ff49f17f54a571e0a8ae750e4895f942cef72
Dmitiry Zub☀️ <dimitryzub@gmail.com>
2022-07-13 08:25:32 +0000

    refactor: udpate thumbnail extraction into a single loop

--- Google/google_news/google_organic_news/google-organic-news.py
+++ Google/google_news/google_organic_news/google-organic-news.py
@@ -15,37 +15,34 @@ params = {
     "q": "gta san andreas",  # search query
     "hl": "en",  # language of the search
     "gl": "us",  # country of the search
-    "num": "100",  # number of search results per page
+    "num": "10",  # number of search results per page
     "tbm": "nws",  # news results
 }
 
 html = requests.get("https://www.google.com/search", headers=headers, params=params, timeout=30)
 selector = Selector(text=html.text)
 
+news_results = []
+
+# extract thumbnails
 all_script_tags = selector.css("script::text").getall()
 
-news_results = []
-decoded_thumbnails = []
+for result, thumbnail_id in zip(selector.css(".xuvV6b"), selector.css(".FAkayc img::attr(id)")):
+    thumbnails = re.findall(r"s=\'([^']+)\'\;var\s?ii\=\['{_id}'\];".format(_id=thumbnail_id.get()), str(all_script_tags))
 
-for _id in selector.css(".YEMaTe img::attr(id)"):
-    # https://regex101.com/r/FZktSD/1
-    thumbnails = re.findall(r"s=\'([^']+)\'\;var\s?ii\=\['{_id}'\];".format(_id=_id.get()), str(all_script_tags))
-
-    decoded_thumbnail = [
+    decoded_thumbnail = "".join([
         bytes(bytes(img, "ascii").decode("unicode-escape"), "ascii").decode("unicode-escape") for img in thumbnails
-    ]
-
-    decoded_thumbnails.append("".join(decoded_thumbnail))
-
-for result, thumbnail_image in zip_longest(selector.css("[jsname=YKoRaf]"), decoded_thumbnails):
+    ])
+    
     news_results.append(
         {
             "title": result.css(".MBeuO::text").get(),
-            "link": result.attr("href"),
+            "link": result.css("a.WlydOe::attr(href)").get(),
             "source": result.css(".NUnG9d span::text").get(),
             "snippet": result.css(".GI74Re::text").get(),
-            "date_published": result.css(".ZE0LJd span:;text").get(),
-            "thumbnail": thumbnail_image
+            "date_published": result.css(".ZE0LJd span::text").get(),
+            "thumbnail": None if decoded_thumbnail == "" else decoded_thumbnail
         }
     )
+
 print(json.dumps(news_results, indent=2, ensure_ascii=False))
